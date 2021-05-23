9b8ac675d3a9456bdae11145c0c8f788179326f3
KarnaukhovKirill <kirya2361@gmail.com>
2021-05-23 10:46:21 +0300

    Изменения

--- src/main/java/ru/job4j/collection/set/SimpleSet.java
+++ src/main/java/ru/job4j/collection/set/SimpleSet.java
@@ -1,22 +1,16 @@
 package ru.job4j.collection.set;
 
 import ru.job4j.collection.SimpleArray;
-
-import java.util.ConcurrentModificationException;
 import java.util.Iterator;
-import java.util.NoSuchElementException;
+import java.util.Objects;
 
 public class SimpleSet<T> implements Set<T> {
     private SimpleArray<T> set = new SimpleArray<>();
-    private int size = 0;
-    private int modCount = 0;
 
     @Override
     public boolean add(T value) {
         if (!contains(value)) {
             set.add(value);
-            size++;
-            modCount++;
             return true;
         }
         return false;
@@ -25,12 +19,8 @@ public class SimpleSet<T> implements Set<T> {
     @Override
     public boolean contains(T value) {
         for (T t : set) {
-            if (t != null) {
-                if (t.equals(value)) {
-                    return true;
-                }
-            } else {
-                return value == null;
+            if (Objects.equals(t, value)) {
+                return true;
             }
         }
         return false;
@@ -38,25 +28,6 @@ public class SimpleSet<T> implements Set<T> {
 
     @Override
     public Iterator<T> iterator() {
-        return new Iterator<>() {
-            int index = 0;
-            private final int expectedMC = modCount;
-
-            @Override
-            public boolean hasNext() {
-            if (expectedMC != modCount) {
-                throw new ConcurrentModificationException();
-            }
-                return index < size;
-            }
-
-            @Override
-            public T next() {
-                if (!hasNext()) {
-                    throw new NoSuchElementException();
-                }
-                return set.get(index++);
-            }
-        };
+        return set.iterator();
     }
 }
