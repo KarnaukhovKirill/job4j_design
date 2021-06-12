6e01aee0385d559d137d1a13facc5d451d151e74
KarnaukhovKirill <kirya2361@gmail.com>
2021-06-12 14:40:49 +0300

    Вывод таблицы умножения в текстовый файл

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,22 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int i = 1; i <= 9; i++) {
+                if (i != 1) {
+                    out.write("\n".getBytes());
+                }
+                for (int j = 1; j <= 9; j++) {
+                    String result = Integer.toString(i * j);
+                    String end = result + " ";
+                    out.write(end.getBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
