1f0448976a5af2bdb430c76ea0a78ebb36d6548d
KarnaukhovKirill <kirya2361@gmail.com>
2021-06-13 11:24:03 +0300

    Исправления

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -7,7 +7,7 @@ public class ResultFile {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
             for (int i = 1; i <= 9; i++) {
                 if (i != 1) {
-                    out.write("\n".getBytes());
+                    out.write(System.lineSeparator().getBytes());
                 }
                 for (int j = 1; j <= 9; j++) {
                     String result = Integer.toString(i * j);
