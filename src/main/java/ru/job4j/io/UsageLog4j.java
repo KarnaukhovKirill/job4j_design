c41eeb4a70ca87df9308b950ab113b0ed40c0a7d
KarnaukhovKirill <kirya2361@gmail.com>
2021-06-20 15:05:41 +0300

    Использование шаблона для подстановки переменных

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,21 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        int age = 33;
+        boolean goodMan = true;
+        double weigth = 78.9;
+        long lvl = 80L;
+        char favoriteNumber = '7';
+        byte countCountry = 100;
+        short population = 30000;
+
+        LOG.debug("int : {}, "
+                + "boolean : {}, "
+                + "double : {}, "
+                + "long : {} "
+                + "char : {} "
+                + "byte : {} "
+                + "short : {} ",
+                age, goodMan, weigth, lvl, favoriteNumber, countCountry, population);
     }
 }
