d33941c640a63ba2d3e307b460c5a04d4507da00
KarnaukhovKirill <kirya2361@gmail.com>
2021-09-26 11:24:05 +0300

    Исправление

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.Calendar;
@@ -40,5 +41,37 @@ public class CinemaTest {
         cinema.add(session);
         var sessions = cinema.find(session1 -> true);
         assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+
+    @Ignore
+    @Test(expected = Exception.class)
+    public void buyWithWrongDate() {
+        Session session = new Session3D();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(1899, 9, 11, 20, 15);
+        assertThat(session, is(new Ticket3D()));
+    }
+
+    @Ignore
+    @Test
+    public void sessionIsNotFound() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> false);
+        assertTrue(sessions.isEmpty());
+    }
+
+    @Ignore
+    @Test(expected = Exception.class)
+    public void buySamePlace() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        Ticket ticket01 = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+        assertThat(ticket01, is(new Ticket3D()));
     }
 }
