ae06cfe40352ce289de0dbf547f06f1d0a283291
KarnaukhovKirill <kirya2361@gmail.com>
2021-11-06 13:54:27 +0300

    убраны ненужные imports

--- src/main/java/ru/job4j/design/srp/Employee.java
+++ src/main/java/ru/job4j/design/srp/Employee.java
@@ -1,7 +1,5 @@
 package ru.job4j.design.srp;
 
-
-import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Calendar;
 import java.util.Objects;
 
--- src/main/java/ru/job4j/design/srp/Employees.java
+++ src/main/java/ru/job4j/design/srp/Employees.java
@@ -1,9 +1,6 @@
 package ru.job4j.design.srp;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.util.ArrayList;
 import java.util.List;
 
 @XmlRootElement(name = "Employees")
